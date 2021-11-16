e9b250b2a6fe4e29be20305ec55a329006932756
Jacob <jacobkwat@gmail.com>
2021-11-15 22:41:34 -0600

    Restyled imports

--- src/components/TimelineThing.js
+++ src/components/TimelineThing.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import '../App.css';
 
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import {
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineOppositeContent,
+} from '@mui/lab';
 
 const TimelineThing = ({ movie }) => {
   const [isClicked, setIsClicked] = useState(false);
