5da3b4f6a589fc6b53edacf8a4c5f7cc82c87ae4
Archana261996 <71586561+Archana261996@users.noreply.github.com>
2021-11-15 22:24:02 -0600

    Made dots clickable

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 
 function App() {
   const [data, loading, error] = useData("/");
-
+  const [isClicked,setIsClicked] = useState(false);
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Error :(</h1>;
   console.log(data[0], "app js data");
   return (
     <div className="App">
 
-<Timeline>
+<Timeline position = 'alternate'>
       {data.map((phase) => {
+        
 return (
         phase['movies'].map((movie) => (
               <TimelineItem>
+                <TimelineOppositeContent>
+            <img height = '100px' src = {movie.url}></img>
+          </TimelineOppositeContent>
               <TimelineSeparator>
-                <TimelineDot />
+                <div onClick = {() => setIsClicked (true)}>
+                <TimelineDot color = {isClicked ? 'success' : 'primary' } />
+                </div>
                 <TimelineConnector />
               </TimelineSeparator>
               <TimelineContent>
-                
                 {movie.name}
-                
                 </TimelineContent>
+                
             </TimelineItem> 
        ) )) } )}
 </Timeline>
